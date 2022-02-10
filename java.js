fd7ebf9a6f33e08c5c766c8c873e835dce4a915e
Marina-Dev <offrinmarina343@gmail.com>
2022-02-10 13:37:26 +0100

    - Add site

---
+++ java.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("sideNavigation").style.marginLeft = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
